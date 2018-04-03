985bd483e9d785620d1e60472b808f4c745e9373
David Yonwook Chung <yon_gospel@hotmail.com>
2018-04-03 15:00:57 -0700

    [components] creates Landing components

---
+++ client/src/components/Landing.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+import Login from "./Login.jsx";
+
+class Landing extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <div>landing</div>;
+  }
+}
+
+export default Landing;
